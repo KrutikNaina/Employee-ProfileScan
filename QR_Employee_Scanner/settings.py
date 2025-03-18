23c6ca5f8f1ba89a2f4e35d369253a8939afedbe
Krutik Naina <nenakrutik2004@gmail.com>
2025-03-18 19:37:45 +0530

    Added WhiteNoise for static file handling

--- QR_Employee_Scanner/settings.py
+++ QR_Employee_Scanner/settings.py
@@ -55,12 +55,14 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # Add this line
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'QR_Employee_Scanner.urls'
